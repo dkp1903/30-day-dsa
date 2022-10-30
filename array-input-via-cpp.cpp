3a4876a540715a1c30f90a75ed4a71a84f48642a
Dushyant Pathak <dushyantpathak6789@gmail.com>
2022-10-30 18:01:54 +0530

    Create array-input-via-cpp.cpp

---
+++ array-input-via-cpp.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> a;
+    int k;
+    for (int i = 0; i < 10; i++) {
+        cin >> k;
+        a.push_back(k);
+    }
+    return 0;
+}
